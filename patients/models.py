572c93592589ecbd9ddd2233cc1ce4b130de1d69
jayboy1115 <ibitoyejohnson234@gmail.com>
2025-05-07 16:46:14 +0100

    My Backend

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from auth.models import User
+from hospitals.models import HospitalBranch
 
-# Create your models here.
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    hospital_branch = models.ForeignKey(HospitalBranch, on_delete=models.SET_NULL, null=True, blank=True)
+    universal_id = models.CharField(max_length=20, unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')], blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return f"{self.user.full_name} ({self.universal_id})"
+
