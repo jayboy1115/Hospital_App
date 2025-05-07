0600ade133d0a87299f74c7b12cd15032ed472ae
jayboy1115 <ibitoyejohnson234@gmail.com>
2025-05-07 16:48:54 +0100

    My Backend

--- hospital_app/settings.py
+++ hospital_app/settings.py
@@ -69,6 +69,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hospital_app.wsgi.application'
 
+AUTH_USER_MODEL = 'auth.User'
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
