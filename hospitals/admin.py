c0dc085ac21450a5ba57137cc60e0a971c93c078
jayboy1115 <ibitoyejohnson234@gmail.com>
2025-05-19 16:20:40 +0100

    My Backend

--- hospitals/admin.py
+++ hospitals/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Hospital, HospitalBranch, Doctor
 
-# Register your models here.
+@admin.register(Hospital)
+class HospitalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'is_verified', 'created_at')
+    list_filter = ('is_verified',)
+    search_fields = ('name', 'email')
+    readonly_fields = ('created_at',)
+
+@admin.register(HospitalBranch)
+class HospitalBranchAdmin(admin.ModelAdmin):
+    list_display = ('name', 'hospital', 'city', 'state', 'created_at')
+    search_fields = ('name', 'hospital__name', 'city', 'state')
+    readonly_fields = ('created_at',)
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'specialization', 'branch', 'created_at')
+    search_fields = ('name', 'specialization', 'branch__name')
+    readonly_fields = ('created_at',)
