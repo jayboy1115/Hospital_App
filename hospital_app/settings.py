3fda098f7c26f4e763433d852991c111c7592290
jayboy1115 <ibitoyejohnson234@gmail.com>
2025-06-11 14:21:28 +0100

    My Backend

--- hospital_app/settings.py
+++ hospital_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'rest_framework_simplejwt',
     'authentication',
     'hospitals',
