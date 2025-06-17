beb4ab1dbf55e09bf023396cbe70f0adb6897055
jayboy1115 <ibitoyejohnson234@gmail.com>
2025-06-17 10:09:43 +0100

    My Backend

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,9 @@
+# chatbot/admin.py
 from django.contrib import admin
+from .models import ChatbotSession
 
-# Register your models here.
+@admin.register(ChatbotSession)
+class ChatbotSessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'patient', 'symptoms', 'created_at')
+    search_fields = ('symptoms', 'patient__universal_id')
+    list_filter = ('created_at',)
